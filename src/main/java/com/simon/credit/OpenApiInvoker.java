fe565181d78022a00398732ed7fa4aac0c42e55b
xuziming <xuziming@touna.cn>
2019-04-09 12:42:03 +0800

    add the openapi method invocation: gjj.credit.getRepayUrl

--- src/main/java/com/simon/credit/OpenApiInvoker.java
+++ src/main/java/com/simon/credit/OpenApiInvoker.java
@@ -13,8 +13,11 @@ import com.simon.credit.util.network.GjjRequestUtils;
 public class OpenApiInvoker {
 
 	public static void main(String[] args) throws Exception {
-		// 2.1前置检测接口
-		testGjjCreditCheckUser();
+		// 2.1 前置检测接口
+		// testGjjCreditCheckUser();
+
+		// 2.9.2 获取合作方还款页面URL接口
+		testGetRepayUrl();
 
 		// 3.1 获取订单额外信息接口
 		// testGjjApiLoadApplyExtInfo();
@@ -44,6 +47,32 @@ public class OpenApiInvoker {
 		// String url  = "https://openapitest.to$$u$$na.cn/gateway/gjj/gjj.credit.checkUser";
 		// String url  = "http://10.0.4.137:8443/gateway/gjj/gjj.credit.checkUser";
 		String url  = "http://127.0.0.1:8443/gateway/gjj/gjj.credit.checkUser";
+
+		// 发送请求
+		JSONObject response = GjjRequestUtils.sendRequest(parseCorrectUrl(url), paramJson);
+		System.out.println("response: " + response);// 解密后的明文
+	}
+
+	/**
+	 * 2.9.2获取合作方还款页面URL接口
+	 * <pre>
+	 * 调用关系: 51公积金调用投哪开发服务
+	 * 51公积金(煎饼网络)业务标准化对接文档地址: https://apizza.net/console/project/6addf1454ea049c2534318c469a3c85b/browse
+	 * </pre>
+	 * @throws IOException
+	 */
+	public static void testGetRepayUrl() throws IOException {
+		JSONObject paramJson = new JSONObject();
+
+		paramJson.put("f"			, "get_repay_url");
+		paramJson.put("apply_id"	, 1111			 );
+		paramJson.put("loan_no"		, "2222"		 );
+		paramJson.put("return_url"	, "https://kaifa.jianbing.com/pt?apply_id=1111&loan_no=2222");
+
+		// 2.12.9.2获取合作方还款页面URL接口(开放平台必须配置开放服务method: gjj.credit.getRepayUrl, 否则会报参数异常)
+		// String url  = "https://openapitest.to$$u$$na.cn/gateway/gjj/gjj.credit.getRepayUrl";
+		// String url  = "http://10.0.4.137:8443/gateway/gjj/gjj.credit.getRepayUrl";
+		String url  = "http://127.0.0.1:8443/gateway/gjj/gjj.credit.getRepayUrl";
 
 		// 发送请求
 		JSONObject response = GjjRequestUtils.sendRequest(parseCorrectUrl(url), paramJson);
