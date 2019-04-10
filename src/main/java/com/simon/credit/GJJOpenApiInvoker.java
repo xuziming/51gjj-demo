102522480aa8a5f8e80b6269dd799738d3a6a94a
xuziming <xuziming@touna.cn>
2019-04-10 20:03:30 +0800

    improve some invocation code

--- src/main/java/com/simon/credit/GJJOpenApiInvoker.java
+++ src/main/java/com/simon/credit/GJJOpenApiInvoker.java
@@ -1,8 +1,12 @@
 package com.simon.credit;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.alibaba.fastjson.JSONObject;
+import com.simon.credit.util.lang.StringUtils;
+import com.simon.credit.util.network.TNHttpUtils;
 import com.simon.credit.util.network.TNRequestUtils;
 
 /**
@@ -14,32 +18,67 @@ public class GJJOpenApiInvoker {
 
 	public static void main(String[] args) throws Exception {
 		// 3.1 获取订单额外信息接口
-		testGjjApiLoadApplyExtInfo();
+		testGjjApiLoadApplyExtInfoMethod1();
+		testGjjApiLoadApplyExtInfoMethod2();
 	}
 
 	/**
-	 * 3.1 获取订单额外信息接口
+	 * 3.1 获取订单额外信息接口(投哪相关系统直接调用51公积金开发平台)
 	 * <pre>
-	 * 调用关系: 投哪系统调用51公积金接口
+	 * 调用关系: TN系统调用51公积金接口
 	 * 51公积金(煎饼网络)业务标准化对接文档地址: https://apizza.net/console/project/6addf1454ea049c2534318c469a3c85b/browse
 	 * </pre>
 	 * @throws IOException
 	 */
-	public static void testGjjApiLoadApplyExtInfo() throws Exception {
+	@Deprecated
+	public static void testGjjApiLoadApplyExtInfoMethod1() throws Exception {
 		JSONObject paramJson = new JSONObject();
 
 		paramJson.put("f"			, "load_apply_ext_info");
-		paramJson.put("product_cid"	, 132);
+		paramJson.put("product_cid"	, "132");
 		paramJson.put("apply_id"	, "5E74977B-3101-5149-92F4-00B53606DE6D");
 		paramJson.put("type"		, "GJJ");
 		paramJson.put("time"		, "2019-04-09 10:00:00");
 
 		// 3.1 获取订单额外信息接口
-		String url  = "https://kaifa.jianbing.com/api/apiBusiness.php";
+		// String url  = "https://kaifa.jianbing.com/api/apiBusiness.php";
+		// String url  = "http://127.0.0.1:8443/gateway/gjj/gjj.credit.loadApplyExtInfo";
+		String url  = "https://openapitest.to$$un$$a.cn/gateway/gjj/gjj.credit.loadApplyExtInfo";
 
 		// 发送请求
-		JSONObject response = TNRequestUtils.tnSendRequest(url, paramJson);
+		JSONObject response = TNRequestUtils.tnSendRequest(parseCorrectUrl(url), paramJson);
 		System.out.println("response: " + response);// 解密后的明文
+	}
+
+	/**
+	 * 3.1 获取订单额外信息接口(TN相关系统调用)
+	 * <pre>
+	 * 调用关系: TN系统调用51公积金接口
+	 * 51公积金(煎饼网络)业务标准化对接文档地址: https://apizza.net/console/project/6addf1454ea049c2534318c469a3c85b/browse
+	 * </pre>
+	 * @throws IOException
+	 */
+	public static void testGjjApiLoadApplyExtInfoMethod2() throws Exception {
+		Map<String, String> paramJson = new HashMap<String, String>();
+
+		paramJson.put("f"			, "load_apply_ext_info");
+		paramJson.put("product_cid"	, "132");
+		paramJson.put("apply_id"	, "5E74977B-3101-5149-92F4-00B53606DE6D");
+		paramJson.put("type"		, "GJJ");
+		paramJson.put("time"		, "2019-04-09 10:00:00");
+
+		// 3.1 获取订单额外信息接口
+		// String url  = "https://kaifa.jianbing.com/api/apiBusiness.php";
+		// String url  = "http://127.0.0.1:8443/gateway/gjj/gjj.credit.loadApplyExtInfo";
+		String url  = "https://openapitest.to$$un$$a.cn/gateway/gjj/gjj.credit.loadApplyExtInfo";
+
+		// 发送请求
+		String response = TNHttpUtils.doPost(parseCorrectUrl(url), paramJson, 5000, 10000);
+		System.out.println("response: " + response);// 解密后的明文
+	}
+
+	private static String parseCorrectUrl(String url) {
+		return StringUtils.replace(url, "$$", "");
 	}
 
 }
