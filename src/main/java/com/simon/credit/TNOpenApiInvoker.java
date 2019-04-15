06aee098fb1f9c37fe42022fc9d061fd5cf25f80
xuziming <xuziming@touna.cn>
2019-04-15 12:33:55 +0800

    improve some code

--- src/main/java/com/simon/credit/TNOpenApiInvoker.java
+++ src/main/java/com/simon/credit/TNOpenApiInvoker.java
@@ -2,6 +2,8 @@ package com.simon.credit;
 
 import java.io.IOException;
 
+import org.apache.commons.codec.digest.DigestUtils;
+
 import com.alibaba.fastjson.JSONObject;
 import com.simon.credit.util.lang.StringUtils;
 import com.simon.credit.util.network.GjjRequestUtils;
@@ -17,7 +19,7 @@ public class TNOpenApiInvoker {
 		testGjjCreditCheckUser();
 
 		// 2.9.2 获取合作方还款页面URL接口
-		testGetRepayUrl();
+		// testGetRepayUrl();
 	}
 
 	/**
@@ -31,14 +33,22 @@ public class TNOpenApiInvoker {
 	public static void testGjjCreditCheckUser() throws IOException {
 		JSONObject paramJson = new JSONObject();
 
+//		String name   = "张三";
+//		String idCard = "420624199409284312";
+//		String phone  = "13912345004";
+
+		String name   = "华明诚";
+		String idCard = "341103198209173712";
+		String phone  = "15812347788";
+
 		paramJson.put("f"			, "check_user"						);
 		paramJson.put("product_cid"	, "3"								);
-		paramJson.put("ID"			, "420624199409284312"			   	);
-		paramJson.put("name"		, "张三"				   				);
-		paramJson.put("id_md5"		, "9b5eaa1f623ed663c9e6969e2b1ed04f");
-		paramJson.put("phone_md5"	, "75a8e965cfd8396de1d8636484d5835f");
-		paramJson.put("id_phone_md5", "311cad615afcf62bfcaf540ae5cce8ad");
-		paramJson.put("phone_id_md5", "46415fb12f05f96cf9e8956ceebb07b2");
+		paramJson.put("ID"			, idCard							);
+		paramJson.put("name"		, name								);
+		paramJson.put("id_md5"		, DigestUtils.md5Hex(idCard)		);
+		paramJson.put("phone_md5"	, DigestUtils.md5Hex(phone)			);
+		paramJson.put("id_phone_md5", DigestUtils.md5Hex(idCard + phone));
+		paramJson.put("phone_id_md5", DigestUtils.md5Hex(phone + idCard));
 
 		// 2.1前置检测接口(开放平台必须配置开放服务method: gjj.credit.checkUser, 否则会报参数异常)
 		String url  = "https://openapitest.to$$u$$na.cn/gateway/gjj/gjj.credit.checkUser";
